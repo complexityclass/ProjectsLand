6b65f5146200a3f3cbc29015bd7cb66654dfd8dc
Valeriy <complexityclass@gmail.com>
2016-01-23 19:11:45 +0300

    ends up with jsx

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,49 @@
 package controllers
 
+import java.util.UUID
+
 import play.api._
+import play.api.libs.json.{JsArray, Json}
 import play.api.mvc._
 
 class Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Lol"))
-  }
+    //constants
+    val JSON_KEY_AUTHOR = "author"
+    val JSON_KEY_TEXT = "text"
+    val JSON_KEY_COMMENTS = "comments"
+    val JSON_KEY_ID = "id"
+
+    //properties
+    var commentsJson: JsArray = Json.arr(
+        Json.obj(
+            JSON_KEY_ID -> UUID.randomUUID().toString,
+            JSON_KEY_AUTHOR -> "Pete Hunt",
+            JSON_KEY_TEXT -> "This is first comment"),
+
+        Json.obj(
+            JSON_KEY_ID -> UUID.randomUUID().toString,
+            JSON_KEY_AUTHOR -> "Michael Jordan",
+            JSON_KEY_TEXT -> "This is another comment")
+    )
+
+    //actions
+    def index = Action {
+        Ok(views.html.projectDashboard())
+    }
+
+    def comments = Action {
+        Ok(commentsJson)
+    }
+
+    def comment(author: String, text: String) = Action {
+        val newComment = Json.obj(
+            JSON_KEY_ID -> UUID.randomUUID().toString,
+            JSON_KEY_AUTHOR -> author,
+            JSON_KEY_TEXT -> text)
+
+        commentsJson = commentsJson :+ newComment
+        Ok(newComment)
+    }
 
 }
