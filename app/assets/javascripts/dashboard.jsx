df438db59d54496f6d9336e2f6108630bf32ce10
Popov Valery <v.popov@rambler-co.ru>
2016-01-24 16:41:01 +0300

    Added simple login form

--- app/assets/javascripts/dashboard.jsx
+++ app/assets/javascripts/dashboard.jsx
@@ -2,7 +2,7 @@ var Profile = React.createClass({
 	render: function() {
 		return (
 			<div style={divStyleProfile}>
-				<h1>Profile</h1>
+				<h1>Projects land</h1>
 			</div>
 		);
 	}
@@ -12,16 +12,69 @@ var Project = React.createClass({
 	render: function() {
 		return (
 			<div>
-				<h1>Project</h1>
+				<h3>Log in</h3>
 			</div>
+		);
+	}
+});
+
+var AuthForm = React.createClass({
+
+	getInitialState: function() {
+		return {value: 'enter login'};
+	},
+	
+	handleChange: function(event) {
+		this.setState({value: event.target.value});
+	},
+
+	handleSubmit: function(event) {
+
+		event.preventDefault();
+		var login = React.findDOMNode(this.refs.login).value.trim();
+		var pass = React.findDOMNode(this.refs.pass).value.trim();
+
+		if (!login || !pass) {
+			return;
+		}
+
+		var authURL = "http://localhost:9000/auth?login=" + login + "&pass=" + pass;
+
+		$.ajax({
+			url: authURL,
+			method: 'POST',
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				console.log(data)
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(authURL, status, err.toString());
+            }.bind(this)
+		});
+
+		React.findDOMNode(this.refs.login).value = '';
+        React.findDOMNode(this.refs.pass).value = '';
+        
+        return;
+	},
+
+
+	render: function() {
+		return (
+			<form className="commentForm" onSubmit={this.handleSubmit}>
+			<input type="text" placeholder="Your login" ref="login" />
+			<input type="text" placeholder="Your password" ref="pass" />
+			<input type="submit" value="go" />
+			</form>
 		);
 	}
 });
 
 var divStyleProfile = {
 	color: 'red',
-	WebkitTransition: 'all', // note the capital 'W' here
-	msTransition: 'all' // 'ms' is the only lowercase vendor prefix
+	WebkitTransition: 'all',
+	msTransition: 'all'
 };
 
 ReactDOM.render(
@@ -32,4 +85,9 @@ ReactDOM.render(
 ReactDOM.render(
 	<Project />,
 	document.getElementById('projects')
+);
+
+ReactDOM.render(
+	<AuthForm />,
+	document.getElementById('loginForm')
 );
--- app/controllers/DashboardController.scala
+++ app/controllers/DashboardController.scala
@@ -8,9 +8,23 @@ import play.api.mvc._
   */
 class DashboardController extends Controller {
 
+    val LOGIN_FIELD = "login"
+    val PASSWD_FIELD = "password"
+
     //actions
     def dashboard = Action {
         Ok(views.html.dashboard())
+    }
+
+    def auth(login: String, pass: String) = Action {
+        println(login + pass)
+
+        val info = Json.obj(
+        LOGIN_FIELD -> login,
+        PASSWD_FIELD -> pass
+        );
+
+        Ok(info)
     }
 
 }
